2969e94bce30942f6734c87409ddaefbc34d466d
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2022-06-11 02:48:46 +0530

    Added banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,18 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="max-w-7xl m-auto">
       <Head>
         <title>PrintIt</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
-      <div></div>
+      <Banner />
     </div>
   );
 };
